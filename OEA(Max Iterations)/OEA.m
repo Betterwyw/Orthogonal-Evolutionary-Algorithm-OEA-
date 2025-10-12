21d742afd8430bc43701f779caf7cdd7b652f959
聪明的墨菲特 <95678383+Betterwyw@users.noreply.github.com>
2025-10-12 15:59:32 +0800

    Add files via upload

---
+++ OEA(Max Iterations)/OEA.m
@@ -0,0 +1,83 @@
+function [Best_Fitness,Best_Pos,Convergence_curve] = OEA(N,Max_iter,lb,ub,dim,fobj)
+
+t=1;
+XPosNew = zeros(N,dim);
+Convergence_curve = zeros(1,Max_iter);
+
+lb = lb.*ones(1,dim);
+ub = ub.*ones(1,dim);
+XPos = lb + rand(N, dim) .* (ub - lb);
+XFit = zeros(1,N);
+
+for i = 1:N
+    XFit(i) = fobj(XPos(i,:));
+end
+
+[Best_Fitness, location] = min(XFit);
+Best_Pos = XPos(location,:);
+
+while t <= Max_iter
+    [XFit, idx] = sort(XFit, 'ascend');
+    XPos = XPos(idx, :);
+ 
+    rank_scores = ((1:N)-(N+1)/2)/(N/2);  
+    k = 2 + 8*(t/ Max_iter);           
+    CR = 1./(1 + exp(-k*rank_scores));    
+    
+    F1 = 0.1 + 0.6*(1/(1+exp(-2.5*(1-2*(t/Max_iter)))));
+    F2 = rand;
+    
+    for i = 1:N
+        jrand = floor(dim*rand + 1);
+        XPosNew(i,:) = XPos(i,:);
+        
+        while true
+            r1 = floor(N * (1 - sqrt(rand))) + 1;
+            if r1 ~= i , break; end 
+        end
+        while true
+            r2 = floor(N * sqrt(rand)) + 1;
+            if r2 ~= i && r2 ~= r1, break; end
+        end
+        
+        v1 = XPos(r2,:) - XPos(i,:);
+        v2 = XPos(r1,:) - XPos(i,:);
+        dot_v1_v1 = dot(v1, v1);
+        
+        if abs(dot_v1_v1) < 1e-20
+            v2_orth = v2; 
+        else
+            dot_v2_v1 = dot(v2, v1);
+            projection = (dot_v2_v1 / dot_v1_v1) * v1;
+            v2_orth = v2 - projection;
+        end
+        update_vector = (F1 * v1) + (F2 * v2_orth);
+        
+        for j = 1:dim
+            if rand < CR(i) || j == jrand
+                XPosNew(i,j) = Best_Pos(j) + update_vector(j);
+            end
+        end
+        
+        flagub = XPosNew(i,:) > ub;
+        flaglb = XPosNew(i,:) < lb;
+        XPosNew(i, flagub) = (XPos(i, flagub) + ub(flagub)) / 2;
+        XPosNew(i, flaglb) = (XPos(i, flaglb) + lb(flaglb)) / 2;
+        
+        new_XFit = fobj(XPosNew(i,:));
+        
+        if new_XFit <= XFit(i)
+            XPos(i,:) = XPosNew(i,:);
+            XFit(i) = new_XFit;
+            if XFit(i) < Best_Fitness
+                Best_Fitness = new_XFit;
+                Best_Pos = XPos(i,:);
+            end
+        end
+    end
+    
+    Convergence_curve(t) = Best_Fitness;
+    t = t+1;
+end
+
+end
